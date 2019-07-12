2e5353debdbf7e542842bd37e74731f0b34c44b7
david <donbarcenagarcia@gmail.com>
2019-07-12 19:21:06 +0200

    Initial comit

---
+++ pokemones/src/main/java/com/pokemon/pokemones/PokemonesApplication.java
@@ -0,0 +1,37 @@
+package com.pokemon.pokemones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+@SpringBootApplication
+public class PokemonesApplication extends Application{
+
+	private ConfigurableApplicationContext context;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void init() throws Exception {
+		context = SpringApplication.run(PokemonesApplication.class, getParameters().getRaw().toArray(new String[0]));	
+	}
+	
+	@Override
+	public void stop() throws Exception {
+		context.close();
+		
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
